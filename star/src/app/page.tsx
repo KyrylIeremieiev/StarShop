578a1ea11cfe3156a68204df1eab9abb9d04fae9
KyrylIeremieiev <34677@ma-web.nl>
2023-11-16 14:14:25 +0100

    change front a bit

--- star/src/app/page.tsx
+++ star/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import Andromeda from '../../public/andromeda.jpg'
+import Sirius from '../../public/sirius.jpg'
+import Betelgeuse from '../../public/betelgeuse.jpeg'
 export default function Home() {
   
   return (
@@ -14,39 +16,59 @@ export default function Home() {
         
       </section>
       <section className="nav--top nav">
-        <h2 className='nav--top__title'>Created by Kyrylo Ieremieiev</h2>
-        <h2 className='nav--top__title nav--top__title--right'>Inspired By Starpeggio</h2>
+        <h2 className='nav--top__title'>$18.000.0000</h2>
+        <h2 className='nav--top__title nav--top__title--right'>Login</h2>
       </section>
       <section className="nav--right nav"></section>
-      <section className="nav--bottom nav"></section>
+      <section className="nav--bottom nav">
+      <h2 className='nav--top__title nav--bottom__title'>Created by Kyrylo Ieremieiev</h2>
+        <h2 className='nav--top__title nav--top__title--right nav--bottom__title'>Inspired By Starpeggio</h2>
+      </section>
 
       <section className='shop'>
         <article className='welcome'>
-          <h1 className={'welcome__title'}>Stars</h1>
-          <figure className='starHolder'><div className='four-pointed-star'></div></figure>
+          <div className='welcome__titleWrap'>
+            <h1 className={'welcome__title'}>Stars</h1>
+            <figure className='starHolder'><div className='four-pointed-star'></div></figure>
+          </div>
+          
+          <p className='welcome__subtext'>We Sell Stars</p>
+          
         </article>
 
         <section className='cat'>
-          <article className="article">
-            <div>
+          <article className="article top">
               <h3 className='article__title'>Andromeda</h3>
-              <em className='article__price'>$19.000.0000.000</em>
-            </div>
-            <div className='imgCarrier'>
-              <Image src={Andromeda} alt='image of Andromeda Galaxy' className='article__img'></Image>
-            </div>
+              <form action="" className='article__form'>
+                <label className='article__price'>$19.000.0000.000</label>
+                <p className="article__order">Order</p>
+              </form>
           </article>
+          <div className='imgCarrier'>
+              <Image src={Andromeda} alt='image of Andromeda Galaxy' className='article__img'></Image>
+          </div>
 
           <article className="article">
-            <div>
-              <h3 className='article__title'>Andromeda</h3>
-            <p>The Andromeda Galaxy is a barred spiral galaxy and is the nearest major galaxy to the Milky Way. It was originally named the Andromeda Nebula and is cataloged as Messier 31, M31, and NGC 224. Andromeda has a diameter of about 46.56 kiloparsecs (152,000 light-years)[8] and is approximately 765 kpc (2.5 million light-years) from Earth.</p>
-            
-            </div>
-            <div className='imgCarrier'>
-              <Image src={Andromeda} alt='image of Andromeda Galaxy' className='article__img'></Image>
-            </div>
+              <h3 className='article__title'>Sirius</h3>
+              <form action="" className='article__form'>
+                <label className='article__price'>$5.000.000.000</label>
+                <p className="article__order">Order</p>
+              </form>
           </article>
+          <div className='imgCarrier'>
+              <Image src={Sirius} alt='image of Sirius Star' className='article__img'></Image>
+          </div>
+
+          <article className="article">
+              <h3 className='article__title'>Betelgeuse</h3>
+              <form action="" className='article__form'>
+                <label className='article__price'>$3.000.000.000</label>
+                <p className="article__order">Order</p>
+              </form>
+          </article>
+          <div className='imgCarrier'>
+              <Image src={Betelgeuse} alt='image of Betelgeuse supernova' className='article__img'></Image>
+          </div>
         </section>
       </section>
       
