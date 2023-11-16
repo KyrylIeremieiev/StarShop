9d5684ff44da9afa1d2a7cea47cc5adf47e156d0
KyrylIeremieiev <34677@ma-web.nl>
2023-11-16 02:49:03 +0100

    add created by/inspired by, fix front

--- star/src/app/layout.tsx
+++ star/src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Glass+Antiqua&family=Playfair+Display:ital,wght@0,500;0,600;1,400;1,500;1,600&family=Sometype+Mono:wght@400;500;600&family=Ubuntu:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet"/>      
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet"></link>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
--- star/src/app/page.tsx
+++ star/src/app/page.tsx
@@ -13,15 +13,16 @@ export default function Home() {
         
       </section>
       <section className="nav--top nav">
-        Star's The Limit
+        <h2 className='nav--top__title'>Created by Kyrylo Ieremieiev</h2>
+        <h2 className='nav--top__title nav--top__title--right'>Inspired By Starpeggio</h2>
       </section>
       <section className="nav--right nav"></section>
       <section className="nav--bottom nav"></section>
-      
+
       <section className='shop'>
         <article className='welcome'>
           <h1 className={'welcome__title'}>Stars</h1>
-          
+          <figure className='starHolder'><div className='four-pointed-star'></div></figure>
         </article>
       </section>
       
